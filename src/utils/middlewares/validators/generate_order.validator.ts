cdfec4081fb9896f0a339b75640c97cbbd138fa1
ssssshel <angelarteaga989@gmail.com>
2022-12-29 14:02:07 -0500

    fix validator

--- src/utils/middlewares/validators/generate_order.validator.ts
+++ src/utils/middlewares/validators/generate_order.validator.ts
@@ -2,8 +2,8 @@ import { check } from "express-validator";
 import { validateCreateRequest } from "../../methods";
 
 export const generateOrderValidator = [
-  check('id_user, final_price').exists().isInt(),
-  check('delivery_date').exists().isDate(),
+  check(['id_user, final_price']).exists().isInt(),
+  check('delivery_date').exists().isString(),
   check('payment_method').exists().isObject(),
   check('shipping_address').exists().isObject(),
 
